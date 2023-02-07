ccaf28ac93680614d29823bc8b695edfbcd4896e
AlexKiing <120453772+AlexKiing@users.noreply.github.com>
2023-02-07 09:08:45 +0000

    Update java.js

--- java.js
+++ java.js
@@ -47,7 +47,7 @@ function displayTemperature(response) {
 }
 
 function search(city) {
-  let apiKey = "5f472b7acba333cd8a035ea85a0d4d4c";
+  let apiKey = "18caa93f9b670e28221fbb28791551f6";
   let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
   axios.get(apiUrl).then(displayTemperature);
 }
@@ -86,4 +86,4 @@ fahrenheitLink.addEventListener("click", displayFahrenheitTemperature);
 let celsiusLink = document.querySelector("#celsius-link");
 celsiusLink.addEventListener("click", displayCelsiusTemperature);
 
-search("New York");
+search("Porto");
